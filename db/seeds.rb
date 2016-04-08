1db0640467197f4a453f63f7734f9e1362c298fd
PhilB-1 <pbarnett31@gmail.com>
2016-04-07 17:32:22 -0700

    Add seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,28 +1,35 @@
 User.delete_all
-Restaurant.delete_all
-Review.delete_all
+Tip.delete_all
+Category.delete_all
 
-users = 20.times.map do
-  User.create!(
-                :email      => Faker::Internet.email,
+
+User.create!(
+                :email      => master@envirotip.com,
                 :password   => 'pass' )
-end
 
-restaurant = 20.times.map do
-  Restaurant.create!(
-    :creator_id => rand(1..20),
-    :name => Faker::Company.name,
-    :cuisine => Faker::Lorem.word,
-    :address => Faker::Address.street_address,
-    :city => Faker::Address.city,
-    :state => Faker::Address.state_abbr,
-    :zip => Faker::Address.zip )
-end
+Tip.create!(
+    :category_id => 1,
+    :user_id => 1,
+    :title => "Use Power Switches!",
+    :description => "Many electronics still draw energy even when off. You can limit this by plugging them into power strips and turning those off when your devices are not in use." )
 
-review = 40.times.map do
-  Review.create!(
-    :user_id => rand(1..20),
-    :restaurant_id => rand(1..20),
-    :rating => rand(1..5),
-    :review => Faker::Lorem.paragraph )
-end
+Tip.create!(
+    :category_id => 3,
+    :user_id => 1,
+    :title => "Use Sides of a Page",
+    :description => "Always print double sided if you can and reuse a sheet printed on one side for something else. It can also be used as scratch paper." )
+
+Tip.create!(
+    :category_id => 2,
+    :user_id => 1,
+    :title => "Shut Off the Shower",
+    :description => "When soaping up in the shower, turn off the shower to conserve water." )
+
+Category.create!(
+    :name => "Energy" )
+
+Category.create!(
+    :name => "Water" )
+
+Category.create!(
+    :name => "Waste" )
